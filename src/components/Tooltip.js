213bea57b1b84953cbc64cf9e6d94964ceb80221
saif0110 <khanmohdtauheed4@gmail.com>
2024-11-29 19:23:04 +0530

    file changed

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -8,12 +8,12 @@ const Tooltip = ({ tip1, tip2 }) => {
 
     return (
         <div>
-            <h2 className='tooltip' style={{ display: mouseEnterH ? "block": "none"}}>
-                <div>{tip1}</div>
+            <h2 className='tooltip' style={{ visibility: mouseEnterH ? "visible": "hidden"}}>
+                <div className='tooltiptext'>{tip1}</div>
             </h2>
             <h2 onMouseEnter={() => setMouseEnterH(true)} onMouseLeave={()=>setMouseEnterH(false)}>Hover over me</h2>
-            <p className='tooltip' style={{ display: mouseEnterP ? "block": "none"}}>
-                <div>{tip2}</div>
+            <p className='tooltip' style={{ visibility: mouseEnterP ? "visible": "hidden"}}>
+                <div className='tooltiptext'>{tip2}</div>
             </p>
             <p onMouseEnter={() => setMouseEnterP(true)} onMouseLeave={()=>setMouseEnterP(false)}>Hover me to see another tooltip.</p>
         </div>
