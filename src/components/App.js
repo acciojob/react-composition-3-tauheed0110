24be72267e3f756822bf8af00ddb7f7d9b38c333
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-02 15:07:29 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './../styles/App.css';
 import Tooltip from "./Tooltip";
+
 const App = () => {
   return (
     <div className="App">
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/Tooltip.css'
+
 const Tooltip =({ text, children})=>{
   const [visible, setVisible] =useState(false);
   const showTooltip =()=>setVisible(true);
